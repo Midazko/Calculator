202a0a0a11e028a8358c6625f31432ca59266f3b
46707 <46707@NYIPPE>
2022-10-21 15:48:13 +0200

    Add project files.

---
+++ kalkylator1/Program.cs
@@ -0,0 +1,31 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        string Gångerindex = "*";
+        bool Körtid = true;
+        List<string[]> Beräkningar = new List<string[]>();
+        while (Körtid)
+        {
+            
+            Console.WriteLine("Välkommen till kalkylatorn\n Välj vad du vill göra:\n" +
+                "[1] Öppna kalkylatorn\n" +
+                "[2] Hämta beräkningar\n" +
+                "[3] Avsluta Kalkylatorn");
+            if (Int32.TryParse(Console.ReadLine(), out int Meny))
+            {
+                switch (Meny)
+                {
+
+                }
+            }
+        }
+    }
+}
+class Kalkylatorn
+{
+  static void Beräkning()
+    {
+    
+    }
+}
